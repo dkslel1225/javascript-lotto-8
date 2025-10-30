23dee408d33b8d38dc3fd181372f1a1c4c508529
jyn <spdlqjdkslel1226@naver.com>
2025-10-30 22:31:11 +0900

    feat: output 함수 작성

---
+++ src/view/outputView.js
@@ -0,0 +1,31 @@
+import { Console } from "@woowacourse/mission-utils";
+import { OUTPUT_MESSAGE } from "../constants/message.js";
+
+export class outputView {
+  printTryNum(tryNum) {
+    Console.print(`\n${tryNum}${OUTPUT_MESSAGE.PURCHASE}`);
+  }
+
+  printTickets(tickets) {
+    tickets.forEach((ticket) => {
+      const ticketStr = ticket.join(", ");
+      Console.print(`[${ticketStr}]`);
+    });
+  }
+
+  printResultTitle() {
+    Console.print(OUTPUT_MESSAGE.RESULT_TITILE);
+  }
+
+  printResultPrize(winningCount) {
+    for (const [rank, count] of Object.entries(winningCount)) {
+      const prize = OUTPUT_MESSAGE.RESULT_PRIZE[rank];
+      const prizeCount = OUTPUT_MESSAGE.RESULT_PRIZE_SUFFIX(count);
+      Console.print(`${prize}${prizeCount}`);
+    }
+  }
+
+  printResultYield(lottoYield) {
+    Console.print(OUTPUT_MESSAGE.RESULT_YIELD(lottoYield));
+  }
+}
